3a2c3f76167f46bc1a0b57a87e19a40d7d5d5003
Julie <julieglion@gmail.com>
2024-10-29 13:00:56 +0100

    update colors

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-[color:--background] bg-black font-sans h-svh relative`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased text-[color:--foreground] bg-[--background] font-sans h-svh relative`}>{children}</body>
     </html>
   );
 }
--- src/components/Cookie-consent-popup.jsx
+++ src/components/Cookie-consent-popup.jsx
@@ -3,9 +3,9 @@ import { LiaCookieBiteSolid } from "react-icons/lia";
 
 const CookieConsentPopup = () => {
   return (
-    <div className="bg-white text-[color:--background] w-[250px] m-6 p-6 rounded-lg flex flex-col gap-4 absolute right-0 bottom-0 z-1">
+    <div className=" bg-[--foreground] text-[color:--background] w-[250px] m-6 p-6 rounded-lg flex flex-col gap-4 absolute right-0 bottom-0 z-1">
       <div className="flex justify-between items-start">
-        <LiaCookieBiteSolid className="text-5xl scale-x-[-1] mb-6" />
+        <LiaCookieBiteSolid className="text-5xl scale-x-[-1] mb-8" />
         <span className="text-2xl leading-6 cursor-pointer">&#215;</span>
       </div>
       <p className="max-w-[15ch]">We use cookies to improve your user experience.</p>
