ccb54ec7ee5f93720f37a4355c597d0c10a8c774
Julie <julieglion@gmail.com>
2024-11-01 11:26:17 +0100

    add state functionality

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
 import CookieConsentPopup from "@/components/CookieConsentPopup";
 
-// import { useState } from "react";
-
 export default function Home() {
   return <CookieConsentPopup />;
 }
--- src/components/CookieConsentPopup.jsx
+++ src/components/CookieConsentPopup.jsx
@@ -1,17 +1,38 @@
-import CookieConsentPopupCTA from "./CookieConsentPopupCTA";
+"use client";
+
+import { useState } from "react";
+
+// import CookieConsentPopupCTA from "./CookieConsentPopupCTA";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 
 const CookieConsentPopup = () => {
-  return (
-    <div className=" bg-[#0a0a0a] text-[#ededed] dark:bg-[#ededed] dark:text-[#0a0a0a] w-[250px] m-6 p-6 rounded-[15px] flex flex-col gap-4 absolute right-0 bottom-0 z-1">
-      <div className="flex justify-between items-start">
-        <LiaCookieBiteSolid className="text-5xl scale-x-[-1] mb-8" />
-        <span className="text-2xl leading-6 cursor-pointer">&#215;</span>
-      </div>
-      <p className="max-w-[15ch] text-xl ">We use cookies to improve your user experience.</p>
+  const [isCookieAccepted, setIsCookieAccepted] = useState(false);
+  const [isCookieConsentPopupOpen, setIsCookieConsentPopupOpen] = useState(true);
 
-      <CookieConsentPopupCTA ctatext="I like Cookies" />
-    </div>
+  return (
+    !isCookieAccepted &&
+    isCookieConsentPopupOpen && (
+      <div className=" bg-[#0a0a0a] text-[#ededed] dark:bg-[#ededed] dark:text-[#0a0a0a] w-[250px] m-6 p-6 rounded-[15px] flex flex-col gap-4 absolute right-0 bottom-0 z-1">
+        <div className="flex justify-between items-start">
+          <LiaCookieBiteSolid className="text-5xl scale-x-[-1] mb-8" />
+          <button
+            onClick={() => {
+              setIsCookieConsentPopupOpen(!isCookieConsentPopupOpen);
+            }}
+            className="text-2xl leading-6 ">
+            &#215;
+          </button>
+        </div>
+        <p className="max-w-[15ch] text-xl ">We use cookies to improve your user experience.</p>
+        <button
+          onClick={() => {
+            setIsCookieAccepted(!isCookieAccepted);
+          }}
+          className=" bg-[#ededed] dark:bg-[#0a0a0a] text-[#0a0a0a] dark:text-[#ededed] rounded-lg px-6 py-3">
+          I like Cookies
+        </button>
+      </div>
+    )
   );
 };
 
